9c8b7705afe69494be60a8607a4b158b3165170e
Taya Hanchar <tayahanchar@gmail.com>
2024-11-19 17:48:40 +0200

    feat: use filter reducer in books list

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -3,10 +3,13 @@ import './booksList.css';
 import Book from './Book';
 import { useDispatch } from 'react-redux';
 import { deleteBook, addToFavorites} from '../books-reducer/actionCreators';
+import {selectTitle, selectAuthor} from '../filterReducer';
 
 function BooksList() {
   const booksList = useSelector((state) => state.booksReducer);
   const dispatch = useDispatch();
+  const titleFilter = useSelector(selectTitle);
+  const authorFilter = useSelector(selectAuthor);
 
   const delBook = (id) => {
     dispatch(deleteBook(id));
@@ -16,12 +19,25 @@ function BooksList() {
     dispatch(addToFavorites(id));
   }
 
+  let filteredBook = booksList;
+
+  if(titleFilter.length) {
+    filteredBook = filteredBook.filter((book) => book.title.toLowerCase().includes(titleFilter.toLowerCase().trim()));
+  }
+
+  if(authorFilter.length) {
+    filteredBook = filteredBook.filter((book) => book.author.toLowerCase().includes(authorFilter.toLowerCase()));
+  }
+  
+
+   
+
   return (
     <div className="books-list-section">
       <h2 className='books-list_title'>My books</h2>
       { booksList.length ?
       <ul className='books-list'>
-        {booksList.map((book, i) => <Book delBook={delBook} addToFavoritesBook={addToFavoritesBook} key={book.id} index={++i} {...book}  />)}
+        {filteredBook.map((book, i) => <Book delBook={delBook} addToFavoritesBook={addToFavoritesBook} key={book.id} index={++i} {...book}  />)}
       </ul> :
       <div style={{textAlign: 'center'}}>There is no books</div>
       }
