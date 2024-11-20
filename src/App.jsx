9c31a85ce8dc3ff19052c2bf0d084d047ee4e7c1
Taya Hanchar <tayahanchar@gmail.com>
2024-11-20 16:55:24 +0200

    feat: add helmet

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,29 @@ import Filters from './components/Filters';
 import Form from './components/Form';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import {Helmet} from "react-helmet";
 
 
 function App() {
 
   return (
-    <Provider store={store}>
-      <header className='header'>
-        <h1>Book library app</h1>
-      </header>
-      <main className='main'>
-        <Form />
-        <section className='books-section'>
-          <Filters />
-          <BooksList />
-        </section>
-      </main>
-    </Provider>
+    <>
+      <Helmet>
+          <meta name='description' content='App with your own library' />
+      </Helmet>
+      <Provider store={store}>
+        <header className='header'>
+          <h1>Book library app</h1>
+        </header>
+        <main className='main'>
+          <Form />
+          <section className='books-section'>
+            <Filters />
+            <BooksList />
+          </section>
+        </main>
+      </Provider>
+    </>
   )
 }
 
